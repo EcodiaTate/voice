d1290e624304329db5919226ae58842298b18afa
EcodiaTate <tate@ecodia.au>
2025-07-14 10:22:32 +1000

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 4173,  // Use the PORT environment variable (Render) or fallback to 4173 for local
+    host: '0.0.0.0',                 // Expose server to all network interfaces
+  }
 })
